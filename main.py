ef27bb8d90ce45bb1b52a7ae2ef58b0bb7d348ac
Jenny Juarez <jj497@cornell.edu>
2023-01-10 12:30:03 -0500

    fix logger

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import time
 
+logging.basicConfig(level=logging.NOTSET)
+
 logging.info('Main.py was called')
-for i in range(50):
+for i in range(10):
     time.sleep(2)
     logging.debug('Test Debug')
     print('Test print')
